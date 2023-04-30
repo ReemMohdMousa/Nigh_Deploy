9562fbeb3a4527ba818541476725992539eaea19
ReemMohdMousa <reem.mohd.mousa@gmail.com>
2023-04-30 17:10:05 +0300

    ...

--- frontend/src/components/NavBar/NavFriendReq.js
+++ frontend/src/components/NavBar/NavFriendReq.js
@@ -129,7 +129,7 @@ export default function BasicMenu() {
   };
 
   useEffect(() => {
-    getAllFriends();
+    // getAllFriends();
     SentRequests();
     ReceivedRequests();
   }, [isAdded, isReceived]);
